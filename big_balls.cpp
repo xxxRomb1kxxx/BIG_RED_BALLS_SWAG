837a00c97bf8647f567b88f83c71bba37e617ed5
Ромочка3 <romanyakushev4@gmail.com>
2024-05-16 21:53:50 +0200

    0% of cringe

---
+++ big_balls.cpp
@@ -0,0 +1,62 @@
+﻿#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+class Ball {
+public:
+    sf::CircleShape shape;
+    float v;
+    float g;
+    float damping;
+    float air_resistance;
+
+    Ball(float r, float initial_x, float initial_y, float g, float damping, float air_resistance) : v(0), g(g), damping(damping), air_resistance(air_resistance) {
+        shape.setRadius(r);
+        shape.setOrigin(sf::Vector2f(r, r));
+        shape.setPosition(initial_x, initial_y);
+        shape.setFillColor(sf::Color::Red);
+
+    }
+    void update(float t, float ground_level) {
+        float a = g - (air_resistance * v  /*(1/shape.getRadius())*/);
+        v += g * t;
+        shape.move(0, v * t);
+
+        if (shape.getPosition().y + shape.getRadius() >= ground_level) {
+            shape.setPosition(shape.getPosition().x, ground_level - shape.getRadius());
+            v = -v * damping;
+        }
+    }
+};
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Big Balls");
+    std::vector<Ball> balls;
+
+    balls.emplace_back(50.f, 500.f, 100.f, 90.8f, 0.7f, 0.1f);
+    balls.emplace_back(100.f, 1000.f, 100.f, 90.8f, 0.8f, 0.1f);
+    balls.emplace_back(150.f, 1500.f, 100.f, 90.8f, 0.9f, 0.1f);
+    sf::RectangleShape ground(sf::Vector2f(1920, 300));
+    ground.setPosition(0, 780);
+    ground.setFillColor(sf::Color::Green);
+    sf::Clock clock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        float t = clock.restart().asSeconds();
+
+        for (auto& ball : balls) {
+            ball.update(t, 780);
+        }
+        window.clear();
+        window.draw(ground);
+        for (const auto& ball : balls) {
+            window.draw(ball.shape);
+        }
+        window.display();
+    }
+    return 0;
+}
